a39a425334d98640a95f7ef5dc59d143220f8863
chenle17 <chenle_17@163.com>
2019-04-22 21:57:04 +0800

    lesson03-03

---
+++ cv_lesson03/swimming_angle.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def get_angle(v, V, S, T):
+    '''
+
+    :param v: 人的速度
+    :param V: 河水速度的数组
+    :param S: 河水宽的数组
+    :param T: 限定时间
+    :return: a，过每条河时的角度
+    '''
+    a = np.zeros(S.shape)
+
+    # a[S - T * v == 0] = 0  # 0度角到对岸刚好T时间
+    # 0度角也到达不了对岸时：
+    a[S - T * v  > 0] = None
+    # T时间到达对岸，游泳的角度：
+    a[S - T * v < 0] = np.arccos(S / (T * v))
+    return a
