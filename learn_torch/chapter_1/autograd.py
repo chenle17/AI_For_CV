14a5ef8dc8d1be2e2aa106b31cac6298e34309a5
chenle <chenle_17@163.com>
2019-06-17 17:46:24 +0800

    torch nn

--- learn_torch/chapter_1/autograd.py
+++ learn_torch/chapter_1/autograd.py
@@ -1,6 +1,7 @@
 import torch
+from torch.autograd import Variable
 
-def tensor():
+def tensor_gradient():
     # requires_grad 为True追踪所有对于该张量的操作
     x = torch.ones(2, 2,requires_grad=True)
     print(x)
@@ -15,6 +16,7 @@ def tensor():
     z = y * y * 3
     print(z)# MulBackward0
     out = z.mean() # MeanBackward1
+    print('----------out-0-------')
     print(out)
     print(out.requires_grad) # True
 
@@ -25,7 +27,9 @@ def tensor():
     a = ((a * 3) / (a - 1))
     print(a)
     print(a.requires_grad)
+    print(a.grad_fn)
     a.requires_grad_(True)
+    print(a)
     print(a.requires_grad)
     print(a.grad_fn)
     b = (a * a).sum()
@@ -33,6 +37,46 @@ def tensor():
     print(b.requires_grad)
     print(b.grad_fn)
 
+    print('------------------------------------x grad')
+
+    print(out.backward)
+    print(x.grad)
+
+def tensor_gradient_2():
+    x = Variable(torch.tensor([[2., 5.]]), requires_grad=True)
+    print(x)
+    y = torch.zeros(1, 2)
+    y[0, 0] = x[0, 0] * 2 + x[0, 1]
+    y[0, 1] = x[0, 1] * 3 + x[0, 0]
+    print(y)
+    # while y.data.norm() < 1000:
+    #     y = y * 2
+
+    print(y.requires_grad)
+
+    # gradients = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
+    # y.backward(gradients)
+    # print(x.grad)
+
+    gradients = torch.ones(1, 2)
+    print(gradients)
+    y.backward(gradients)
+    print(x.grad)
+
+
+    print(x.requires_grad)
+    print((x ** 2).requires_grad)
+
+    with torch.no_grad():
+        print((x ** 2).requires_grad)
+        print(x.requires_grad)
+
+
+
 if __name__ == '__main__':
-    print('-----------------------------------tensor---------')
-    tensor()
+    # print('-----------------------------------tensor_grad---------')
+    # tensor_gradient()
+
+    print('-----------------------------------tensor_grad_2---------')
+    tensor_gradient_2()
+
